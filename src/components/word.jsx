b088c19fd3be1497dd0b64c1dec4a2dfb5f80e82
deryana <derana765@gmail.com>
2024-08-28 13:22:36 +0700

    landing page 1.0 abydos school update blog foto muter

--- src/components/word.jsx
+++ src/components/word.jsx
@@ -5,36 +5,44 @@ import Nonomi from "../assets/nonomi.jpeg";
 
 const Word = () => {
   return (
-    <div className="word" data-aos="fade-up" id="blog">
-      <div className="word-content">
-        <img src={Hoshino} alt="hoshino" className="word-img" />
-        <div className="word-content-img"></div>
-        <div className="word-content-text">
-          <i className="fa-solid fa-quote-left"></i>
-          <p>
-            Abydos is really helped me a lot, i would highly recomended Abydos.
-          </p>
-          <h3>Takanashi Hoshino</h3>
+    <div data-aos="fade-up" id="blog">
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="word">
+        <div className="word-content">
+          <img src={Hoshino} alt="hoshino" className="word-img" />
+          <div className="word-content-img"></div>
+          <div className="word-content-text">
+            <i className="fa-solid fa-quote-left"></i>
+            <p>
+              Abydos is really helped me a lot, i would highly recomended
+              Abydos.
+            </p>
+            <h3>Takanashi Hoshino</h3>
+          </div>
         </div>
-      </div>
-      <div className="word-content">
-        <img src={Sensei} alt="hoshino" className="word-img" />
-        <div className="word-content-img"></div>
-        <div className="word-content-text">
-          <i className="fa-solid fa-quote-left"></i>
-          <p>Di Abydos kalian bisa ngoding ges, gaskan daftar</p>
-          <h3>Gwejh (SENESI)</h3>
+        <div className="word-content">
+          <img src={Sensei} alt="hoshino" className="word-img" />
+          <div className="word-content-img"></div>
+          <div className="word-content-text">
+            <i className="fa-solid fa-quote-left"></i>
+            <p>Di Abydos kalian bisa ngoding ges, gaskan daftar</p>
+            <h3>Gwejh (SENESI)</h3>
+          </div>
         </div>
-      </div>
-      <div className="word-content">
-        <img src={Nonomi} alt="hoshino" className="word-img" />
-        <div className="word-content-img"></div>
-        <div className="word-content-text">
-          <i className="fa-solid fa-quote-left"></i>
-          <p>
-            Abydos is really helped me a lot, i would highly recomended Abydos.
-          </p>
-          <h3>Izayoi Nonomi</h3>
+        <div className="word-content">
+          <img src={Nonomi} alt="hoshino" className="word-img" />
+          <div className="word-content-img"></div>
+          <div className="word-content-text">
+            <i className="fa-solid fa-quote-left"></i>
+            <p>
+              Abydos is really helped me a lot, i would highly recomended
+              Abydos.
+            </p>
+            <h3>Izayoi Nonomi</h3>
+          </div>
         </div>
       </div>
     </div>
